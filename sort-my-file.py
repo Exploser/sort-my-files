f77d20839f3b2ce7717c2bed7fb92c0048d84f68
Exploser <exploserbusiness@gmail.com>
2024-06-26 21:38:45 -0400

    Update script to take move or copy arguments

--- sort-my-file.py
+++ sort-my-file.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import argparse
 
-def organize_files(directory, ignore_files):
+def organize_files(directory, ignore_files, exclude_extensions, mode):
     # Get the absolute path of the directory
     abs_directory = os.path.abspath(directory)
 
@@ -15,26 +15,32 @@ def organize_files(directory, ignore_files):
             
             # Get file extension
             file_extension = os.path.splitext(file)[1][1:]  # Remove the dot
-            if not file_extension:
-                continue  # Skip files without extension
+            if not file_extension or file_extension in exclude_extensions:
+                continue  # Skip files without extension or with excluded extensions
 
             # Create the directory for the file extension if it doesn't exist
             extension_dir = os.path.join(abs_directory, file_extension)
             if not os.path.exists(extension_dir):
                 os.makedirs(extension_dir)
 
-            # Move the file to the new directory
+            # Move or copy the file to the new directory
             source_path = os.path.join(root, file)
             destination_path = os.path.join(extension_dir, file)
-            shutil.move(source_path, destination_path)
-            print(f'Moved {file} to {extension_dir}')
+            if mode == 'move':
+                shutil.move(source_path, destination_path)
+                print(f'Moved {file} to {extension_dir}')
+            elif mode == 'copy':
+                shutil.copy2(source_path, destination_path)
+                print(f'Copied {file} to {extension_dir}')
 
 if __name__ == "__main__":
     # Set up argument parsing
     parser = argparse.ArgumentParser(description='Organize files by extension in a directory tree.')
     parser.add_argument('directory', help='The directory to organize')
     parser.add_argument('--ignore', nargs='*', default=[], help='Files to ignore')
+    parser.add_argument('--exclude-extensions', nargs='*', default=[], help='File extensions to exclude')
+    parser.add_argument('--mode', choices=['move', 'copy'], default='move', help='Mode to organize files: move or copy')
     args = parser.parse_args()
 
-    organize_files(args.directory, args.ignore)
+    organize_files(args.directory, args.ignore, args.exclude_extensions, args.mode)
 
